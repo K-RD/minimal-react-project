32fe4b1a378ec51f5cb3f1fbdf5e768c8fce59f2
ROBIND KUMAR <kumarrobind386@gmail.com>
2024-05-03 09:40:18 +0530

    Implemented Full screen btn

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 
 const home = () => {
+    const enterFullscreen = () => {
+        const element = document.documentElement;
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if (element.mozRequestFullScreen) { /* Firefox */
+            element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+            element.webkitRequestFullscreen();
+        } else if (element.msRequestFullscreen) { /* IE/Edge */
+            element.msRequestFullscreen();
+        }
+    };
   return (
       <>
           <div className="container">
@@ -9,6 +21,7 @@ const home = () => {
                   <div className="subContainer">
                       <a href="/register"><button >Click here to Register</button></a>
                       <a href="/login"><button >Click here to Login</button></a>
+                      <button type="button" onClick={enterFullscreen}>Enter Fullscreen</button>
                   </div>
               </div>
           </div>
